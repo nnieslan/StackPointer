74987cf73e40070a892efb072e5122b79458aa08
Nathan LeViere <nateleviere@gmail.com>
2012-09-23 16:02:22 -0400

    adding LinkedInInterface class

---
+++ src/java/stackpointer/jobs/LinkedInInterface.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackpointer.jobs;
+
+/**
+ *
+ * @author Nathan
+ */
+public class LinkedInInterface {
+    
+}
