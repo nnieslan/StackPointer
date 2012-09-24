c92691d4e823cf9acc83762501d6bee6761e95ed
Andrew <kellera8@gmail.com>
2012-09-23 21:02:10 -0400

    added answers table

--- database/create.sql
+++ database/create.sql
@@ -1,4 +1,6 @@
 -- This file assumes the existance of a MySQL database named 'stackpointer'.
+-- For the sake of consistency please make a user called 'spuser' (for stackpointer user) and assign a password of 'pass'.
+--
 -- This script can be run as follows:
 --     1) Open up a command prompt and login to the database
 --           mysql -u<username> -p<password> stackpointer
@@ -6,11 +8,22 @@
 --           source C:\path\to\file\create.sql
 
 -- Drop all of the tables if they already exist.
+drop table if exists answers;
 drop table if exists questions;
 
 -- Questions
 create table questions (
-	qid bigint not null,
+	qid bigint not null auto_increment,
 	question_text text not null,
 	primary key(qid)
+);
+
+-- Answers
+create table answers (
+	aid bigint not null auto_increment,
+	answer_text text not null,
+	qid bigint not null,
+	constraint fk_answers_question foreign key (qid)
+	references questions(qid),
+	primary key (aid)
 );
