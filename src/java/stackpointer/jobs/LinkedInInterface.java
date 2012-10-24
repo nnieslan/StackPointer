8c960d9699a4ff45a49d38b08999783c70956cd4
Joe <josephlawsonj@gmail.com>
2012-10-25 00:52:50 +0100

    Added Connection Interface to LinkedIN Pulling Jobs Results in JSON

--- src/java/stackpointer/jobs/LinkedInInterface.java
+++ src/java/stackpointer/jobs/LinkedInInterface.java
@@ -1,28 +1,94 @@
 package stackpointer.jobs;
 
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Date;
-import stackpointer.common.Location;
+import java.util.Scanner;
+import org.scribe.builder.*;
+import org.scribe.builder.api.*;
+import org.scribe.model.*;
+import org.scribe.oauth.*;
+import stackpointer.database.DatabaseConnectionInfo;
+import stackpointer.database.MySQLDatabaseFacade;
 
 /**
  * This class is to interact with the Linked In API, including retrieval
  * of data, and parsing it.
- * @author Nathan
+ * @author JOE
  */
 public class LinkedInInterface {
     private boolean connected;
     private ArrayList<JobPosting> allJobPostings;
+    MySQLDatabaseFacade db = new MySQLDatabaseFacade(DatabaseConnectionInfo.createDefault());
+    private static final String PROTECTED_RESOURCE_URL = "http://api.linkedin.com/v1/job-search:(jobs,facets)?facet=location,us:100";
+    
+    public static void main(String[] args)
+   {
+     OAuthService service = new ServiceBuilder()
+                                .provider(LinkedInApi.class)
+                                .apiKey("v6xty7mvo61a")
+                                .apiSecret("N6Ggy9kfyJc3fVO0")
+                                .build();
+    Scanner in = new Scanner(System.in);
+    
+    System.out.println("=== LinkedIn's OAuth Workflow ===");
+    System.out.println();
 
-    LinkedInInterface()
+    // Obtain the Request Token
+    System.out.println("Fetching the Request Token...");
+    Token requestToken = service.getRequestToken();
+    System.out.println("Retrieved Request Token!");
+    System.out.println();
+
+    System.out.println("Authorize Scribe:");
+    System.out.println(service.getAuthorizationUrl(requestToken));
+    System.out.println("Paste Verifier");
+    System.out.print(">>");
+    Verifier verifier = new Verifier(in.nextLine());
+    System.out.println();
+
+    // Trade the Request Token and Verfier for the Access Token
+    System.out.println("Trading the Request Token for an Access Token...");
+    Token accessToken = service.getAccessToken(requestToken, verifier);
+    System.out.println("Got the Access Token!");
+    System.out.println("(Access Token: " + accessToken + " )");
+    System.out.println();
+
+    // Access Linked In URL!
+    System.out.println("Contact API URL");
+    OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+    
+    // Pull results in JSON format
+    request.addHeader("x-li-format", "json");
+    service.signRequest(accessToken, request);
+    Response response = request.send();
+    System.out.println(response.getBody());    
+ }
+    
+    public LinkedInInterface()
     {
-        //TODO - Initialize values
+    //TODO - Initialize values
         allJobPostings = new ArrayList<JobPosting>();
     }
 
     void establishConnection()
     {
+        OAuthService service = new ServiceBuilder()
+                                .provider(LinkedInApi.class)
+                                .apiKey("v6xty7mvo61a")
+                                .apiSecret("N6Ggy9kfyJc3fVO0")
+                                .callback ("http://localhost:8080/StackPointer/")
+                                .build();
         //TODO - input connection steps and set boolean result value
-        connected = false;
+        connected = true;
     }
 
     //Simple getter function to ensure that connection is valid
@@ -49,8 +115,8 @@ public class LinkedInInterface {
     void updateJobPostings()
     {
         //TODO - access database, grab 100 questions, push onto list
-        allJobPostings.add(new JobPosting(new Location(0,0,0), new Date(), "Headline1", "Desc1", "Company1"));
-        allJobPostings.add(new JobPosting(new Location(0,0,0), new Date(), "Headline1", "Desc1", "Company1"));
+  //      allJobPostings.add(new JobPosting(new Location(0,0,0), new Date(), "Headline1", "Desc1", "Company1"));
+   //     allJobPostings.add(new JobPosting(new Location(0,0,0), new Date(), "Headline1", "Desc1", "Company1"));
     }
 
     //Return the top 100 questions
