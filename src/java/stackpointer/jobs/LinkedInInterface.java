a3e908bf070bfa10379210fd873ec31c6d636e72
Joe <josephlawsonj@gmail.com>
2012-11-26 01:51:25 +0000

    Removed unused functions.

--- src/java/stackpointer/jobs/LinkedInInterface.java
+++ src/java/stackpointer/jobs/LinkedInInterface.java
@@ -148,7 +148,6 @@ public class LinkedInInterface {
                         jJob.getJSONObject("postingDate").getInt("day"));       
                 toAdd.setDatePosted(jobDate);
                 parsed.add(toAdd);
-                //getJobLocations(jJob);
             }
         }
         catch (JSONException e)
@@ -270,26 +269,6 @@ public class LinkedInInterface {
  
           return verifier;
       }
-    
-    public static ArrayList<JobPosting> getJobLocations(JSONObject json)
-    {
-        ArrayList<JobPosting> locations = new ArrayList<JobPosting>();
-        try
-        {
-            JSONArray jobs = json.getJSONArray("values");
-            for(int j=0; j<jobs.length(); j++)
-            {
-                JSONObject jJob = jobs.getJSONObject(j);
-                //System.out.println(GoogleMapsInterface.geocode(jJob.getString("locationDescription")));
-                System.out.println(GoogleMapsInterface.geocode(jJob.getString("locationDescription")));             
-            }
-        }
-        catch (JSONException e)
-        {
-            System.out.println("Error parsing Job locations: "+e);
-        }
-        return locations;
-    }
     
    //Function to update current values from LinkedIn in local database
    //boolean cleanDatabase()
